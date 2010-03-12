7a41ecab8b02c041209f0159e4a84c0f32dd7186
eartle <michaelc@last.fm>
2010-03-12 12:24:29 +0000

    Fixing linker problems on Windows.

--- lib/unicorn/Updater/PluginList.h
+++ lib/unicorn/Updater/PluginList.h
@@ -7,37 +7,9 @@
 class UNICORN_DLLEXPORT PluginList : public GenPluginList
 {
 public:
-    QList<IPluginInfo*> availablePlugins() const
-    {
-        QList<IPluginInfo*> ret;
-        foreach( IPluginInfo* plugin, *this ) {
-            if( plugin->isInstalled() ||
-                !plugin->isPlatformSupported() ) continue;
-            if( plugin->isAppInstalled() )
-                ret << plugin;
-        }
-        return ret;
-    }
-
-    QList<IPluginInfo*> installedPlugins() const
-    {
-        QList<IPluginInfo*> ret;
-        foreach( IPluginInfo* plugin, *this ) {
-            if( plugin->isInstalled())
-                ret << plugin;
-        }
-        return ret;
-    }
-
-    QList<IPluginInfo*> bootstrappablePlugins() const
-    {
-        QList<IPluginInfo*> ret = installedPlugins();
-        foreach( IPluginInfo* plugin, ret ) {
-            if( !plugin->canBootstrap())
-                ret.removeAll( plugin );
-        }
-        return ret;
-    }
+    QList<IPluginInfo*> availablePlugins() const;
+    QList<IPluginInfo*> installedPlugins() const;
+    QList<IPluginInfo*> bootstrappablePlugins() const;
 };
 
 #endif //PLUGIN_LIST_H_
