5ebf9c703dfe0abed0f25ebfb0bbabc291c2c710
Michael Coffey <michaelc@last.fm>
2010-07-22 15:31:52 +0100

    Improve the recent tracks widget
    - Now has a default image for when there is no album art
    - We now only animate once we have tried to load all album arts in a batch
    - We now draw items that haven't been loaded yet outside of the geometry properly
    
    - Also made the HttpImageWidget not an inline class (I hate nmake)

--- lib/unicorn/widgets/HttpImageWidget.h
+++ lib/unicorn/widgets/HttpImageWidget.h
@@ -1,3 +1,23 @@
+/*
+   Copyright 2005-2009 Last.fm Ltd.
+      - Primarily authored by Jono Cole and Micahel Coffey
+
+   This file is part of the Last.fm Desktop Application Suite.
+
+   lastfm-desktop is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   lastfm-desktop is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with lastfm-desktop.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef HTTP_IMAGE_WIDGET_H_
 #define HTTP_IMAGE_WIDGET_H_
 
@@ -10,73 +30,23 @@
 
 #include <lastfm/ws.h>
 
-class UNICORN_DLLEXPORT HttpImageWidget : public QLabel {
-Q_OBJECT
+class UNICORN_DLLEXPORT HttpImageWidget : public QLabel
+{
+    Q_OBJECT
 public:
-    HttpImageWidget( QWidget* parent = 0 )
-    :QLabel( parent ), m_mouseDown( false ){}
+    HttpImageWidget( QWidget* parent = 0 );
 
 public slots:
-    void loadUrl( const QUrl& url, bool gradient = false )
-    {
-        m_gradient = gradient;
-        clear();
-        connect( lastfm::nam()->get(QNetworkRequest(url)), SIGNAL(finished()), SLOT(onUrlLoaded()));
-    }
-
-    void setHref( const QUrl& url )
-    {
-        m_href = url;
-        if( m_href.isValid()) {
-            setCursor( Qt::PointingHandCursor );
-            connect( this, SIGNAL(clicked()), SLOT(onClick()));
-        } else {
-            setCursor( Qt::ArrowCursor );
-            disconnect( this, SIGNAL( clicked()), this, SLOT(onClick()));
-        }
-    }
-
+    void loadUrl( const QUrl& url, bool gradient = false );
+    void setHref( const QUrl& url );
     
 protected:
-    virtual void mousePressEvent( QMouseEvent* event )
-    {
-        m_mouseDown = true;
-    }
-
-    virtual void mouseReleaseEvent( QMouseEvent* event )
-    {
-        if( m_mouseDown &&
-            contentsRect().contains( event->pos() )) emit clicked();
-        
-        m_mouseDown = false;
-    }
+    virtual void mousePressEvent( QMouseEvent* event );
+    virtual void mouseReleaseEvent( QMouseEvent* event );
 
 private slots:
-    void onClick()
-    {
-        QDesktopServices::openUrl( m_href);
-    }
-
-    void onUrlLoaded()
-    {
-        QPixmap px;
-        px.loadFromData(static_cast<QNetworkReply*>(sender())->readAll());
-        
-        if( m_gradient ) {
-            QLinearGradient g(QPoint(), px.rect().bottomLeft());
-            g.setColorAt( 0.0, QColor(0, 0, 0, 0.11*255));
-            g.setColorAt( 1.0, QColor(0, 0, 0, 0.88*255));
-
-            QPainter p(&px);
-            p.setCompositionMode(QPainter::CompositionMode_Multiply);
-            p.fillRect(px.rect(), g);
-            p.end();
-        }
-
-        setPixmap(px);
-
-        emit loaded();
-    }
+    void onClick();
+    void onUrlLoaded();
 
 signals:
     void clicked();
