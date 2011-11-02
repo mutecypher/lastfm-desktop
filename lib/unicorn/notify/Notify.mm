4cac91a915f91000e8cf1c66b7a036b47f5f07f1
Michael Coffey <michaelc@last.fm>
2011-11-02 15:25:01 +0000

    Growl notifications work properly on mac.  Need to bundle the growl framework nicely, Need to make it work on Windows.

--- lib/unicorn/notify/Notify.mm
+++ lib/unicorn/notify/Notify.mm
@@ -40,14 +40,14 @@ unicorn::Notify::Notify(QObject *parent) :
 }
 
 void
-unicorn::Notify::newTrack( const lastfm::Track& track  )
+unicorn::Notify::newTrack( const lastfm::Track& track )
 {
     QString title = track.toString();
     QString description = tr("from %1").arg( track.album() );
 
 #ifdef Q_OS_MAC
-    NSString* nsTitle = [NSString initWithCharacters:(const unichar *)title.unicode() length:(NSUInteger)title.length() ];
-    NSString* nsDescription = [NSString initWithCharacters:(const unichar *)description.unicode() length:(NSUInteger)description.length() ];
+    NSString* nsTitle = [NSString stringWithCharacters:(const unichar *)title.unicode() length:(NSUInteger)title.length() ];
+    NSString* nsDescription = [NSString stringWithCharacters:(const unichar *)description.unicode() length:(NSUInteger)description.length() ];
 
     // TODO: Do the growl notification here. It'll be great!
     [GrowlApplicationBridge notifyWithTitle:(NSString *)nsTitle
