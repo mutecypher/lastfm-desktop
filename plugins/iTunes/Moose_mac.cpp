c011642dbb6dca8304b3c5b46528e1b94141e2fc
Michael Coffey <michaelc@last.fm>
2011-12-23 16:02:52 +0000

    Some changes. Bump the version number. Remove unfinished edit bio label. Don't steal focus for iPod scrobbling.

--- plugins/iTunes/Moose_mac.cpp
+++ plugins/iTunes/Moose_mac.cpp
@@ -154,7 +154,7 @@ Moose::launchAudioscrobbler( const std::vector<std::string>& vargs )
     
     LSApplicationParameters params;
     params.version = 0;
-    params.flags = kLSLaunchDontSwitch;
+    params.flags = kLSLaunchAndHide | kLSLaunchDontSwitch | kLSLaunchAsync;;
     params.application = &appRef;
     params.asyncLaunchRefCon = NULL;
     params.environment = NULL;
