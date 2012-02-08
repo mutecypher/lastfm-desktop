8a673bbee2cfb76997cbb0e399c9e075e9cf4968
Michael Coffey <michaelc@last.fm>
2012-02-08 18:32:37 +0000

    Make Mac users close iTunes to update the plugin.

---
+++ app/client/Dialogs/CloseAppsDialog_mac.mm
@@ -0,0 +1,34 @@
+#include "CloseAppsDialog.h"
+
+QString qt_mac_NSStringToQString(const NSString *nsstr)
+{
+    NSRange range;
+    range.location = 0;
+    range.length = [nsstr length];
+
+    unichar *chars = new unichar[range.length];
+    [nsstr getCharacters:chars range:range];
+    QString result = QString::fromUtf16(chars, range.length);
+    delete[] chars;
+    return result;
+}
+
+QStringList
+CloseAppsDialog::runningApps()
+{
+    QStringList apps;
+
+    // make sure iTunes isn't running
+    NSArray* runningApps = [[NSWorkspace sharedWorkspace] runningApplications];
+
+    for ( int i = 0 ; i <  [runningApps count] ; ++i )
+    {
+        if ( [[[runningApps objectAtIndex:i] bundleIdentifier] isEqualToString:@"com.apple.iTunes"] )
+        {
+            NSString* appName = [[runningApps objectAtIndex:i] localizedName];
+            apps << qt_mac_NSStringToQString( appName );
+        }
+    }
+
+    return apps;
+}
