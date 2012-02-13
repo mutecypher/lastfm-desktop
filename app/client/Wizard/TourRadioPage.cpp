c8e7bab0edfa4469383bd804f4502099f9bac9f2
Michael Coffey <michaelc@last.fm>
2012-02-13 16:31:55 +0000

    Updates to the wizard copy

--- app/client/Wizard/TourRadioPage.cpp
+++ app/client/Wizard/TourRadioPage.cpp
@@ -32,15 +32,16 @@ TourRadioPage::initializePage()
 {
     if ( wizard()->user().isSubscriber() )
     {
-        setTitle( tr( "Listen to non-stop, personalised radio." ) );
-        ui.description->setText( "<p>Use the Last.fm Desktop App to listen to non-stop personalised radio based on the music you want to hear.</p>"
-                                 "<p>Every play of every Last.fm station is totally different, from stations based on aritsts and tags to brand new recommendations tailored to your music taste.</p>" );
+        setTitle( tr( "Listen to non-stop, personalised radio" ) );
+        ui.description->setText( "<p>Use the Last.fm Desktop App to listen to personalised radio based on the music you want to hear.</p>"
+                                 "<p>Every play of every Last.fm station is totally different, from stations based on artists and tags to brand new recommendations tailored to your music taste.</p>" );
     }
     else
     {
-        setTitle( tr( "Subscribe and listen to non-stop, personalised radio." ) );
-        ui.description->setText( "<p>Subscribe to Last.fm for just [3/$3/3 localised] a month and listen to radio using the Last.fm Desktop App.</p>"
-                                 "<p>Every play of every Last.fm station is totally different, from stations based on aritsts and tags to brand new recommendations tailored to your music taste.</p>" );
+        setTitle( tr( "Subscribe and listen to non-stop, personalised radio*" ) );
+        ui.description->setText( "<p>Subscribe to Last.fm and use the Last.fm Desktop App to listen to personalised radio based on the music you want to hear.</p>"
+                                 "<p>Every play of every Last.fm station is totally different, from stations based on aritsts and tags to brand new recommendations tailored to your music taste.</p>"
+                                 "<p>* Radio is a subscriber-only feature outside of the UK, US and Germany</p>" );
 
         QAbstractButton* custom = wizard()->setButton( FirstRunWizard::CustomButton, tr( "Subscribe" ) );
         connect( custom, SIGNAL(clicked()), SLOT(subscribe()));
