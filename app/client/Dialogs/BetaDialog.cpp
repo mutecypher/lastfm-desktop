ae2893cae63225dee7f9f56d44281aa9118cabd8
Michael Coffey <michaelc@last.fm>
2012-02-14 13:43:37 +0000

    Change the beta link to the Audioscrobbler Beta group page.

--- app/client/Dialogs/BetaDialog.cpp
+++ app/client/Dialogs/BetaDialog.cpp
@@ -27,8 +27,8 @@ BetaDialog::BetaDialog(QWidget *parent) :
     }
     else
     {
-        ui->createIssue->setText( tr( "Visit User Voice" ) );
-        ui->feedback->setText( "If you've' noticed a problem, please visit out User Voice page and leave us some feedback. Thanks!" );
+        ui->createIssue->setText( tr( "Visit Audioscrobbler Beta group" ) );
+        ui->feedback->setText( "If you've noticed a problem, please visit our group and leave us some feedback. Thanks!" );
     }
 
     connect( ui->createIssue, SIGNAL(clicked()), SLOT(createIssue()) );
@@ -40,7 +40,7 @@ BetaDialog::createIssue()
     if ( aApp->currentSession()->userInfo().type() == lastfm::User::TypeStaff )
         unicorn::DesktopServices::openUrl( QUrl( "https://jira.last.fm/secure/CreateIssue.jspa?pid=10011&issuetype=1&Create=Create" ) );
     else
-        unicorn::DesktopServices::openUrl( QUrl( "http://lastfmdesktop.uservoice.com/" ) );
+        unicorn::DesktopServices::openUrl( QUrl( "http://www.last.fm/group/Audioscrobbler+Beta" ) );
 }
 
 BetaDialog::~BetaDialog()
