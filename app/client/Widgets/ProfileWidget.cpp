dffd0516e2cec6bab21388119a31598b7d35b86c
Michael Coffey <michaelc@last.fm>
2012-03-28 19:09:37 +0100

    Add a stretch to the profile widget so that it doesn't stretch when the window gets bigger.

--- app/client/Widgets/ProfileWidget.cpp
+++ app/client/Widgets/ProfileWidget.cpp
@@ -129,6 +129,8 @@ ProfileWidget::changeUser( const QString& newUsername )
             ui.topOverallArtists->setObjectName( "section" );
         }
 
+        layout->addStretch();
+
         lastfm::User user = lastfm::User( newUsername );
 
         connect( user.getLovedTracks( 1 ), SIGNAL(finished()), SLOT(onGotLovedTracks()) );
